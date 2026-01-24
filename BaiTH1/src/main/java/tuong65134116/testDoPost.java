11241b2ad1b28e3bbb3a03dc876f35dbfb21fce0
TuongNG59 <nhuynhtuong59@gmail.com>
2026-01-24 17:04:36 +0700

    Create testDoPost

---
+++ BaiTH1/src/main/java/tuong65134116/testDoPost.java
@@ -0,0 +1,32 @@
+package tuong65134116;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class testDoPost
+ */
+@WebServlet("/testDoPost")
+public class testDoPost extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public testDoPost() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
