0407107851534c92dbb6133819b9ba5445333dd9
TuongNG59 <nhuynhtuong59@gmail.com>
2026-01-27 14:58:53 +0700

    chỉnh sửa AboutMe, code BMI.java, bmi.html và bmi.css

--- BaiTH1/src/main/java/tuong65134116/BMI.java
+++ BaiTH1/src/main/java/tuong65134116/BMI.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class BMI
@@ -26,16 +27,36 @@ public class BMI extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/bmi.html").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/html; charset=UTF-8");
+		
+		double h = Double.parseDouble(request.getParameter("height"));
+        double w = Double.parseDouble(request.getParameter("weight"));
+        
+        double bmi = w / (h * h);
+        
+        String kq;
+        if (bmi < 18.5) {
+            kq = "Gầy";
+        } else if (bmi < 25) {
+            kq = "Bình thường";
+        } else if (bmi < 30) {
+            kq = "Thừa cân";
+        } else {
+            kq = "Béo phì";
+        }
+        
+        PrintWriter out = response.getWriter();
+        out.println("<h2>KẾT QUẢ BMI</h2>");
+        out.println("BMI = " + bmi + "<br>");
+        out.println("Kết luận: " + kq + "<br><br>");
+        out.println("<a href='BMI'>Quay lại</a>");
 	}
 
 }
