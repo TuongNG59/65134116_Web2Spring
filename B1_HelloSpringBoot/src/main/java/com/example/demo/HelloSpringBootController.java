0083030cfb6427f1c518550e52f06e7e86033780
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-08 10:36:37 +0700

    Chỉnh sửa tên controller

--- B1_HelloSpringBoot/src/main/java/com/example/demo/HelloSpringBootController.java
+++ B1_HelloSpringBoot/src/main/java/com/example/demo/HelloSpringBootController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloSpringBoot {
+public class HelloSpringBootController {
     @GetMapping("/") // Nếu người dùng request tới địa chỉ "/"
     public String index() {
         return "index"; // Trả về file index.html
