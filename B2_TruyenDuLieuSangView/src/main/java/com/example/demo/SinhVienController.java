401e15f220cf6bf587db123e23f31a9dd6f18fd5
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-08 10:45:56 +0700

    Thêm sinhVienView.html

---
+++ B2_TruyenDuLieuSangView/src/main/java/com/example/demo/SinhVienController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class SinhVienController {
+	@GetMapping("/sinhvien")
+	public String sinhVienInfo(Model model) {
+        model.addAttribute("mssv", "65134116");
+        model.addAttribute("hoten", "Nguyen Huynh Tuong");
+        model.addAttribute("namsinh", "2005");
+        model.addAttribute("gioitinh", "Nam");
+        
+        return "sinhVienView";
+	}
+}
