723431e220b029c5366e86a82acea52f88788c24
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-08 13:04:33 +0700

    First Commit

---
+++ B6_TruyenDanhSach2View/src/test/java/com/example/demo/B6TruyenDanhSach2ViewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B6TruyenDanhSach2ViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
