ae93e446b083e31978f47ad12ccbad41ce03f94e
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-08 10:30:11 +0700

    First Commit

---
+++ B1_HelloSpringBoot/src/main/java/com/example/demo/HelloSpringBoot.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloSpringBoot {
+    @GetMapping("/") // Nếu người dùng request tới địa chỉ "/"
+    public String index() {
+        return "index"; // Trả về file index.html
+    }
+}
