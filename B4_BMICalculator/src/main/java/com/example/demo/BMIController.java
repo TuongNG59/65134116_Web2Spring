cfe7b82e3e45bbf7310c5c51b42c6b279c5dba4d
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-08 11:44:34 +0700

    BMI controller

---
+++ B4_BMICalculator/src/main/java/com/example/demo/BMIController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+
+@Controller
+public class BMIController {
+	@GetMapping("/bmi")
+	public String showForm() {
+		return "bmiForm";
+	}
+	
+	@PostMapping("/bmi")
+	public String calculateBMI(
+			@RequestParam("chieuCao") double chieuCao,
+			@RequestParam("canNang") double canNang,
+			Model model) {
+		
+		double bmi = canNang / (chieuCao * canNang);
+		
+		String ketQua;
+		
+		if(bmi<18.5) {
+			ketQua = "Gầy";
+		}else if(bmi<25) {
+			ketQua = "Bình Thường";
+		}else if(bmi<30) {
+			ketQua = "Thừa Cân";
+		}else {
+			ketQua = "Béo Phì";
+		}
+		
+		model.addAttribute("bmi", String.format("%.2f", bmi));
+		model.addAttribute("ketqua", ketQua);
+		
+		return "resultBMI";
+	}
+	
+}
