d1752429310fe5a5035affe8d701508365a8fd3e
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-11 16:11:37 +0700

    Update HomeController

--- NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/controllers/HomeController.java
+++ NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/controllers/HomeController.java
@@ -1,9 +1,9 @@
 package thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.controllers;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.models.Page65134116;
@@ -16,23 +16,32 @@ public class HomeController {
     ArrayList<Page65134116> dsTrang = new ArrayList<Page65134116>();
     ArrayList<Post65134116> dsPost = new ArrayList<Post65134116>();
     
-	@GetMapping("/")
+    public HomeController(){
+
+        dsTrang.add(new Page65134116(0,"Home","home","Trang chủ",0));
+        dsTrang.add(new Page65134116(1,"About","about","Giới thiệu",0));
+        dsTrang.add(new Page65134116(2,"Contact","contact","Liên hệ",0));
+
+        dsPost.add(new Post65134116(0,"Java","Học Java cơ bản",001));
+        dsPost.add(new Post65134116(1,"Spring","Học Spring Boot",001));
+        dsPost.add(new Post65134116(2,"Thymeleaf","Template engine",002));
+
+    }
+    
+    
+    @GetMapping("/")
 	public String Home() {
 		return "Home";
 	}
-	
-    public HomeController(){
+    
+    
+    @GetMapping("/page/all")
+    public String getAllPage(ModelMap m){
 
-        dsTrang.add(new Page65134116(001,"Home","home","Trang chủ",0));
-        dsTrang.add(new Page65134116(002,"About","about","Giới thiệu",0));
-        dsTrang.add(new Page65134116(003,"Contact","contact","Liên hệ",0));
+        m.addAttribute("lstPages", dsTrang);
 
-        dsPost.add(new Post65134116(01,"Java","Học Java cơ bản",001));
-        dsPost.add(new Post65134116(02,"Spring","Học Spring Boot",001));
-        dsPost.add(new Post65134116(03,"Thymeleaf","Template engine",002));
-
+        return "allpage";
     }
-
 }
 
 
