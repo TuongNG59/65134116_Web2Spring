3ab093719a5c8457326b814e1ca014c0a14e7133
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-11 14:41:29 +0700

    Tạo bài thi thử giữa kì

---
+++ NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/NguyenHuynhTuong65134116FitCmsApplication.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NguyenHuynhTuong65134116FitCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenHuynhTuong65134116FitCmsApplication.class, args);
+	}
+
+}
---
+++ NguyenHuynhTuong65134116_fitCMS/src/test/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/NguyenHuynhTuong65134116FitCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenHuynhTuong65134116FitCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
