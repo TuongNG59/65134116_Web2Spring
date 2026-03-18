6432893d5071d129d3889f70943d0c6c1e7ca6d3
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-18 15:44:11 +0700

    Code cho RESTAPIController

--- NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/controllers/RESTAPIPageController.java
+++ NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/controllers/RESTAPIPageController.java
@@ -1,5 +1,23 @@
 package thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.controllers;
 
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.models.Page65134116;
+
+@RestController
 public class RESTAPIPageController {
 
+    ArrayList<Page65134116> dsTrang = new ArrayList<>();
+
+    public RESTAPIPageController() {
+        dsTrang.add(new Page65134116(0,"TenTrang","TuKhoa","Noidung",0));
+    }
+
+    @GetMapping("/api/page")
+    public ArrayList<Page65134116> getAllPage() {
+        return dsTrang;
+    }
 }
