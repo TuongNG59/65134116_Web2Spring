3a20dc0a73c2384089e70398df632b35d3634b2c
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-18 16:02:29 +0700

    Code PageService

--- NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/services/PageService.java
+++ NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/services/PageService.java
@@ -1,5 +1,18 @@
 package thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.services;
 
-public class PageService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.models.Page65134116;
+import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.repositories.PageRepository;
+
+@Service
+public class PageService {
+	@Autowired
+	PageRepository pRepos;
+	List<Page65134116> getAllPage(){
+		return pRepos.findAll();
+	}
 }
