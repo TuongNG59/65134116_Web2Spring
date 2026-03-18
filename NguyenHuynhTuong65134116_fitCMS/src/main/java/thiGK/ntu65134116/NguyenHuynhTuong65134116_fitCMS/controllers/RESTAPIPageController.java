5d6e00cdf99a3fa3812734f7c11af0251e625643
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-18 16:06:27 +0700

    Sửa RESTAPIController

--- NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/controllers/RESTAPIPageController.java
+++ NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/controllers/RESTAPIPageController.java
@@ -1,28 +1,30 @@
 package thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.controllers;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.models.Page65134116;
-import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.repositories.PageRepository;
+import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.services.PageService;
 
 @RestController
 public class RESTAPIPageController {
-
+	
+	@Autowired
+    PageService pService;
+	
     ArrayList<Page65134116> dsTrang = new ArrayList<>();
 
-    public RESTAPIPageController() {
-        dsTrang.add(new Page65134116(0,"TenTrang","TuKhoa","Noidung",0));
-    }
-
-    @GetMapping("/api/page/all")
-    public ArrayList<Page65134116> getAllPage() {
-        return dsTrang;
+    @GetMapping("/restAPI/page/all")
+    public List<Page65134116> getALLPage(){
+    	List<Page65134116> dsTrang = new ArrayList<Page65134116>();
+    	
+    	dsTrang = pService.getAllPage();
+    	return dsTrang;
     }
     
-    @Autowired
-    PageRepository pRepost;
+    
 }
