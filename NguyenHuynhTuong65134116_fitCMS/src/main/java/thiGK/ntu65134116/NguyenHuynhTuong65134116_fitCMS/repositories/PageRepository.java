73490e1e5a82c25c4938c043d8bd62162d6f7678
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-18 15:37:16 +0700

    Cập nhật PageRepository

--- NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/repositories/PageRepository.java
+++ NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/repositories/PageRepository.java
@@ -1,5 +1,11 @@
 package thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.repositories;
 
-public class PageRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.models.Page65134116;
+
+@Repository
+public interface PageRepository extends JpaRepository<Page65134116, Integer> {
+	
 }
