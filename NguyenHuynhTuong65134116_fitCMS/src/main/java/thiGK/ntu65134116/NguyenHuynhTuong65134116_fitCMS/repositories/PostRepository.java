39b0a295349f73e899209212870c87544ca715b3
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-24 22:36:28 +0700

    Update PostRepository.java

--- NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/repositories/PostRepository.java
+++ NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/repositories/PostRepository.java
@@ -1,5 +1,10 @@
 package thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.repositories;
 
-public class PostRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.models.Post65134116;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post65134116, Integer> {
 }
