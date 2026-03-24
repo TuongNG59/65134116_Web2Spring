61f12116f028d5257371ea9f4b761bc2f0f7dfb7
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-24 22:39:58 +0700

    Xem danh sách Post

--- NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/controllers/RESAPIPostController.java
+++ NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/controllers/RESAPIPostController.java
@@ -1,5 +1,27 @@
 package thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.controllers;
 
-public class RESAPIPostController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.models.Post65134116;
+import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.services.PostService;
+
+@RestController
+public class RESAPIPostController {
+	@Autowired
+    PostService pService;
+
+    @GetMapping("/restAPI/post/all")
+    public List<Post65134116> getAllPost() {
+        return pService.getAllPost();
+    }
+
+    @GetMapping("/restAPI/post/{id}")
+    public Post65134116 getPost(@PathVariable int id) {
+        return pService.getPostById(id);
+    }
 }
--- NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/services/PostService.java
+++ NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/services/PostService.java
@@ -1,5 +1,25 @@
 package thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.models.Post65134116;
+import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.repositories.PostRepository;
+
+@Service
 public class PostService {
+
+    @Autowired
+    PostRepository pRepos;
+
+    public List<Post65134116> getAllPost() {
+        return pRepos.findAll();
+    }
+
+    public Post65134116 getPostById(int id) {
+        return pRepos.findById(id).orElse(null);
+    }
 
 }
