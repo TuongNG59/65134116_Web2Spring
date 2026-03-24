624594917b94421475d0aef96ea74524075932e0
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-24 22:14:28 +0700

    cập nhật PageService

--- NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/services/PageService.java
+++ NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/services/PageService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
 import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.models.Page65134116;
 import thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.repositories.PageRepository;
 
@@ -17,7 +18,7 @@ public class PageService {
 	}
 	
 	public Page65134116 getPageById(int id) {
-		return pRepos.getById(id);
+		return pRepos.findById(id).orElse(null);
 //		return pRepos.findById(id).orElse(null);
 	}
 	
@@ -25,6 +26,7 @@ public class PageService {
 	    return pRepos.save(page);
 	}
 	
+	@Transactional
 	public void deletePage(int id) {
 	    if (!pRepos.existsById(id)) {
 	        throw new RuntimeException("ID không tồn tại");
