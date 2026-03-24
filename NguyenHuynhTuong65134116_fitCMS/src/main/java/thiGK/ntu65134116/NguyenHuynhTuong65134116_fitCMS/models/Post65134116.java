03ffd558ee964a392def238fcc7dc10d61d9ccec
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-24 22:34:33 +0700

    Cập nhật Post65134116

--- NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/models/Post65134116.java
+++ NguyenHuynhTuong65134116_fitCMS/src/main/java/thiGK/ntu65134116/NguyenHuynhTuong65134116_fitCMS/models/Post65134116.java
@@ -1,34 +1,68 @@
 package thiGK.ntu65134116.NguyenHuynhTuong65134116_fitCMS.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "posts")
 public class Post65134116 {
-	private int id;
-	private String title;
-	private String content;
-	private int categoryId;
-	
-	public Post65134116(int id, String title, String content, int categoryId) {
-		super();
-		this.id = id;
-		this.title = title;
-		this.content = content;
-		this.categoryId = categoryId;
-	}
 
-	public int getId() {
-		return id;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-	public String getTitle() {
-		return title;
-	}
+    @Column(name = "title")
+    private String title;
 
-	public String getContent() {
-		return content;
-	}
+    @Column(name = "content")
+    private String content;
 
-	public int getCategoryId() {
-		return categoryId;
-	}
-	
-	
+    @Column(name = "category_id")
+    private int categoryId;
+
+    public Post65134116() {
+    }
+
+    public Post65134116(int id, String title, String content, int categoryId) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.categoryId = categoryId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
 }
