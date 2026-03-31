19272b47c32f75cc127e5e7ea2bbb0dd81446f9f
TuongNG59 <nhuynhtuong59@gmail.com>
2026-03-31 13:37:36 +0700

    Done

---
+++ _pasted_code_/src/snippet/Snippet.java
@@ -0,0 +1,8 @@
+package snippet;
+
+public class Snippet {
+	spring.jpa.hibernate.ddl-auto=none
+	spring.jpa.show-sql=true
+	spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQLDialect
+}
+
