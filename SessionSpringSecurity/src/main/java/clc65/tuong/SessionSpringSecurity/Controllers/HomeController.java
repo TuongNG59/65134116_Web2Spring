c8fe5db31eb162e69ff07d675b0104f6dfe36571
TuongNG59 <nhuynhtuong59@gmail.com>
2026-04-08 10:22:09 +0700

    Code for HomeController and Home.html

--- SessionSpringSecurity/src/main/java/clc65/tuong/SessionSpringSecurity/Controllers/HomeController.java
+++ SessionSpringSecurity/src/main/java/clc65/tuong/SessionSpringSecurity/Controllers/HomeController.java
@@ -1,5 +1,18 @@
 package clc65.tuong.SessionSpringSecurity.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
