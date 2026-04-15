e95c1019f4d0caf6c0397f4f2700cb2d2546599d
TuongNG59 <nhuynhtuong59@gmail.com>
2026-04-15 15:34:57 +0700

    Code LoaiTinTucService

--- QLTinTuc/src/main/java/clc65/tuong/qltintuc/Services/LoaiTinTucService.java
+++ QLTinTuc/src/main/java/clc65/tuong/qltintuc/Services/LoaiTinTucService.java
@@ -1,5 +1,20 @@
 package clc65.tuong.qltintuc.Services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import clc65.tuong.qltintuc.Models.LoaiTinTuc;
+import clc65.tuong.qltintuc.Repositories.LoaiTinTucRepository;
+
+@Service
 public class LoaiTinTucService {
 
+    @Autowired
+    private LoaiTinTucRepository loaiTinTucRepo;
+
+    public List<LoaiTinTuc> getAll() {
+        return loaiTinTucRepo.findAll();
+    }
 }
