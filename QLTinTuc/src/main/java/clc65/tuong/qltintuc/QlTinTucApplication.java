1a9a4b74fb47b4de8c50af91ebed91a5bbd7e64c
TuongNG59 <nhuynhtuong59@gmail.com>
2026-04-15 15:08:08 +0700

    First Commit

---
+++ QLTinTuc/src/main/java/clc65/tuong/qltintuc/QlTinTucApplication.java
@@ -0,0 +1,13 @@
+package clc65.tuong.qltintuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlTinTucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlTinTucApplication.class, args);
+	}
+
+}
---
+++ QLTinTuc/src/test/java/clc65/tuong/qltintuc/QlTinTucApplicationTests.java
@@ -0,0 +1,13 @@
+package clc65.tuong.qltintuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlTinTucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
