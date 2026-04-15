8b607f67891fb7c1a5db622799fdfa079c772819
TuongNG59 <nhuynhtuong59@gmail.com>
2026-04-15 16:00:54 +0700

    Xử lý xoá

--- QLTinTuc/src/main/java/clc65/tuong/qltintuc/Controllers/AdminController.java
+++ QLTinTuc/src/main/java/clc65/tuong/qltintuc/Controllers/AdminController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -40,5 +41,10 @@ public class AdminController {
         tinTucService.save(tinTuc);
         return "redirect:/admin/tin-tuc";
     }
-
+    
+    @GetMapping("/tintuc/xoa/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        tinTucService.delete(id);
+        return "redirect:/admin/tin-tuc";
+    }
 }
