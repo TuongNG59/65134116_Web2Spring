7ccdd430632b92b990292bbbc6247d47b42fb4f9
TuongNG59 <nhuynhtuong59@gmail.com>
2026-04-15 15:41:30 +0700

    Sửa TinTucRepository vì import sai models, code TinTucService

--- QLTinTuc/src/main/java/clc65/tuong/qltintuc/Repositories/TinTucRepository.java
+++ QLTinTuc/src/main/java/clc65/tuong/qltintuc/Repositories/TinTucRepository.java
@@ -3,9 +3,9 @@ package clc65.tuong.qltintuc.Repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import clc65.tuong.qltintuc.Models.LoaiTinTuc;
+import clc65.tuong.qltintuc.Models.TinTuc;
 
 @Repository
-public interface TinTucRepository extends JpaRepository<LoaiTinTuc, Long> {
-	
+public interface TinTucRepository extends JpaRepository<TinTuc, Long> {
+
 }
--- QLTinTuc/src/main/java/clc65/tuong/qltintuc/Services/TinTucService.java
+++ QLTinTuc/src/main/java/clc65/tuong/qltintuc/Services/TinTucService.java
@@ -1,5 +1,32 @@
 package clc65.tuong.qltintuc.Services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import clc65.tuong.qltintuc.Models.TinTuc;
+import clc65.tuong.qltintuc.Repositories.TinTucRepository;
+
+@Service
 public class TinTucService {
 
+    @Autowired
+    private TinTucRepository tinTucRepo;
+
+    public List<TinTuc> getAll() {
+        return tinTucRepo.findAll();
+    }
+
+    public TinTuc getById(Long id) {
+        return tinTucRepo.findById(id).orElse(null);
+    }
+
+    public void save(TinTuc tin) {
+        tinTucRepo.save(tin);
+    }
+
+    public void delete(Long id) {
+        tinTucRepo.deleteById(id);
+    }
 }
