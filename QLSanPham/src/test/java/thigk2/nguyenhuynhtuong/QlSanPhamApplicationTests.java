d827099008a55152db893cdcbd36445fd09f39cb
TuongNG59 <nhuynhtuong59@gmail.com>
2026-04-24 13:46:15 +0700

    Lần thứ nhất

---
+++ QLSanPham/src/test/java/thigk2/nguyenhuynhtuong/QlSanPhamApplicationTests.java
@@ -0,0 +1,13 @@
+package thigk2.nguyenhuynhtuong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlSanPhamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
